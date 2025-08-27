c55a882180618baba3e209b2593bbe7edb400531
Mandeep Bhandari <mandeepbhandari892@gmail.com>
2025-08-27 17:18:34 +0545

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+# Use your custom style
+plt.style.use("./deeplearning.mplstyle")
 
-print(pandas.__version__)
+x_train = np.array([1.0, 2.0])
+y_train = np.array([300, 500])
+m = len(x_train)
+print(f"Number of traning example is: {m}")
